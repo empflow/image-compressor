84cbd07e03d1bf63aa798b5a82d8598729b3321f
Matvey <morozovmatvej75@gmail.com>
2023-06-22 13:17:20 +0300

    add more build & start commands & remove unused code
    removed logging an array of strings 'done!!!' like 44 times

--- src/index.ts
+++ src/index.ts
@@ -8,21 +8,18 @@ import createResultsDir from "./createResultsDir";
 import populateResultsDir from "./populateResultsDir";
 
 // 'result' is a directory with an 'init' image and a 'result' image
-const initFilesDir = getEnvVar("INIT_DIR_PATH");
-const resultsFilesDir = getEnvVar("RESULTS_DIR_PATH");
 
-const initImgsFileNames = fs.readdirSync(initFilesDir);
-rmRfDir(resultsFilesDir);
+console.log(getEnvVar("MODE"));
 
-console.time("timer");
 (async () => {
-  const promises = initImgsFileNames.map((initImgFileName) => {
+  const initFilesDir = getEnvVar("INIT_DIR_PATH");
+  const resultsFilesDir = getEnvVar("RESULTS_DIR_PATH");
+  const initImgsFileNames = fs.readdirSync(initFilesDir);
+  rmRfDir(resultsFilesDir);
+
+  initImgsFileNames.forEach((initImgFileName) => {
     const paths = getNeededPaths(initImgFileName);
     createResultsDir(paths);
     return populateResultsDir(paths);
   });
-
-  const result = await Promise.all(promises);
-  console.log(result);
 })();
-console.timeEnd("timer");
